1c2291052dbef982ac6c0db7f79ddb0fdbdd1e27
wangx <wangx_0102@126.com>
2020-05-16 19:22:39 +0800

    add upload the spj and src functions

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,13 @@
-FROM ubuntu:16.04
-MAINTAINER wangxinsheng "wangx_0102@126.com"
-# 换uestc的源
-COPY ./config/sources.list /etc/apt/
-COPY ./config/get-pip.py .
-RUN apt-get update
-# 安装允许环境
-RUN apt-get install -y python python3.5  gcc g++ $buildDeps && \
-    apt-get install -y openjdk-8-jdk curl python3-pip
-RUN cp /usr/bin/python /usr/bin/python_bak &&\
-    rm /usr/bin/python &&\
-    ln -s /usr/bin/python3.5 /usr/bin/python
-ADD . /code
-WORKDIR /code
-RUN pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple  -r  requirements.txt
-RUN python run.py
-EXPOSE 5000
-
-
+#基于的基础镜像
+FROM python:3
+ 
+#代码添加到code文件夹
+ADD . /usr/src/app
+ 
+# 设置app文件夹是工作目录
+WORKDIR /usr/src/app
+ 
+# 安装支持
+RUN pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --no-cache-dir -r requirements.txt
+ 
+CMD [ "python", "/usr/src/app/run.py" ]
--- run.py
+++ run.py
@@ -6,31 +6,107 @@ from App import app
 import os
 from flask import request
 
+error = {
+    "code": 5000,
+    "msg": ""
+}
+
+
+def clean_files(dist):
+    os.system(f"rm -rf {dist}")
+
+
 '''
-判题 
+judge problem
 '''
 
 
 @app.route('/judge')
 def judge():
+    # The source file position
     src = request.args.get("src")
+    # The task max running time
     time_limit = request.args.get("time")
+    # The task max using memory
     mem_limit = request.args.get("mem")
+    # The result file is stored position
     dist = request.args.get("dist")
+    # Is using special judge 1 stand for using and 0 stand for not using
+    spj_state = request.args.get("spj")
+    # Language for spj 1 is c language and 2 is c++ language
+    spj_lang = request.args.get("spjLang")
+    # The  sudo password, if you are root ignoring it.
     sudo_password = '818923'
-    command = f'./Core -c {src} -t {time_limit} -m {mem_limit}  -d {dist}'
+    command = './Core'
+    # Add running args
+    if src != "":
+        command.join(f" -c {src}")
+    elif time_limit != "":
+        command.join(f" -t {time_limit}")
+    elif mem_limit != "":
+        command.join(f" -m {mem_limit}")
+    elif dist != "":
+        command.join(f" -d {dist}")
+    elif spj_state == "1":
+        command.join(f" -s")
+        if spj_lang != "":
+            command.join(f" -S {spj_lang}")
+
     app.logger.info("running the command: %s", command)
+    # Running the command
     os.system('echo %s|sudo -S %s' % (sudo_password, command))
+    # Read the result
     with open(f"{dist}/result.txt", "r") as f:
         line = f.readline().strip()
         time_cost = f.readline().strip()
         mem_cost = f.readline().strip()
+        f.close()
+    # The result dic
     data = {
         "result": line,
         "time": time_cost,
         "memory": mem_cost
     }
+    # Complete and clean temp files
+    clean_files(dist)
     return data
+
+
+@app.route('/src', methods='POST')
+def get_src():
+    dist = request.form['dist']
+    src = request.form['src']
+    lang = request.form['lang']
+    if lang == "1":
+        file_name = "main.c"
+    elif lang == "2":
+        file_name = "main.cpp"
+    elif lang == "3":
+        file_name = "Main.java"
+    else:
+        error['msg'] = "error language option"
+        return error
+    with open(dist + file_name, "w", encoding="utf-8") as f:
+        f.write(src)
+        f.close()
+
+
+@app.route("spj", methods=['POST'])
+def get_spj():
+    dist = request.form['dist']
+    spj_src = request.form['spj']
+    spj_lang = request.form['lang']
+    if spj_lang == "1":
+        file_name = "main.c"
+    elif spj_lang == "2":
+        file_name = "main.cpp"
+    else:
+        error['msg'] = "error language option"
+        return error
+    with open(dist + file_name, "w", encoding="utf-8") as f:
+        f.write(spj_src)
+        f.close()
+    os.system(f"g++ {dist + file_name} -o SpecialJudge -O2")
 
 
 if __name__ == "__main__":
